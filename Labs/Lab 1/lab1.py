7da38edad58694c89339c33fa7f041f124a8f080
xusamson8 <xusamson14@gmail.com>
2024-01-31 15:28:03 -0800

    lab1 py commit w tests

--- Labs/Lab 1/lab1.py
+++ Labs/Lab 1/lab1.py
@@ -1,5 +1,6 @@
+
 class lab1:
-    def twoSum(self, nums, target) :
+    def twoSum(self, nums , target ):
         numMap = {}
         n = len(nums)
 
@@ -8,7 +9,20 @@ class lab1:
 
         for i in range(n):
             complement = target - nums[i]
-            if complement in numMap and numMap[complement] != i 
+            if complement in numMap and numMap[complement] != i :
                 return [i, numMap[complement]]
             
         return []
+    
+#test cases below
+nums1 = [2, 7, 11, 15]
+target1 = 9
+print(lab1().twoSum(nums1, target1))
+
+nums2 = [3, 5, 8, 12]
+target2 = 7
+print(lab1().twoSum(nums2, target2))
+
+nums5 = [1000000, 2000000, 3000000, 4000000]
+target5 = 5000000
+print(lab1().twoSum(nums5, target5))
