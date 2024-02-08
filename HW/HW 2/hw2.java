f109e1b85b9d7add6b4d7bd46a7b149c5c3ee156
xusamson8 <xusamson14@gmail.com>
2024-02-07 17:09:01 -0800

    hw2 commit

--- HW/HW 2/hw2.java
+++ HW/HW 2/hw2.java
@@ -1,5 +1,15 @@
 public class hw2 {
-    public boolean isBad(int version){
-
+    //You are given an API bool isBadVersion(version) which returns whether version is bad. 
+    public boolean firstBadVer(int n){
+        int low = 1, high = n;
+        while(low <= high){    //loop exits when low > high
+            int mid = low + (high - low)/2;
+            if(isBadVersion(mid)){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return low;
     }
 }
