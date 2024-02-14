aa64eda733bf659ea6514cb9f96dc18d42a8e566
xusamson8 <xusamson14@gmail.com>
2024-02-13 16:36:24 -0800

    extra credit

--- Extra Credit/"Third lightbulb's the charm"/Fibonacci.java
+++ Extra Credit/"Third lightbulb's the charm"/Fibonacci.java
@@ -7,10 +7,39 @@ public class Fibonacci {
             return 1;
         }
 
-        for( int i = 2; i < n ; i++){
+        //if it isnt the bases cases then move onto the loop
+        int a = 0;
+        int b = 1; //starting with the first two numnbers of fibonacci
 
+        for( int i = 2; i <= n ; i++){
+        //this loop gets the next number by summing up the previous elements, starting with 2
+        //
+            int next = a+b;
+            a = b;
+            b = next;
         }           
-        
+        return b;
+
     }         
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(5));
+        System.out.println(fibonacci(10));
+        System.out.println(fibonacci(9));
+        System.out.println(fibonacci(0));
+        System.out.println(fibonacci(1));
+    }
+
+    //explanation on the time complextity 
+    /* 
+     * the if statements, initializations , and return statements that are not within the for loop each are check a total 
+     * of one time
+     * 
+     * we can calculate that the time complexity of this implementation is O(N), 
+     * 
+     * 
+     * 
+     */
+
 }
 
--- Extra Credit/"Third lightbulb's the charm"/Fibonacci.py
+++ Extra Credit/"Third lightbulb's the charm"/Fibonacci.py
@@ -1,3 +1,18 @@
 class Fibonacci:
     def fibonacci(self, n):
+        #Base Cases
+        if n==0:
+            return 0
+        elif n ==1:
+            return 1
         
+        a,b = 0,1
+        for i in range (2,n+1):
+            a = b
+            b = a+b
+
+        return b
+
+print(Fibonacci().fibonacci(10))
+
+
