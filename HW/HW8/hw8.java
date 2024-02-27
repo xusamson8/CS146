863a0fc4790fe52018a285a015a1c38af87bc4e0
xusamson8 <xusamson14@gmail.com>
2024-02-26 16:25:55 -0800

    hw 8 commit

--- HW/HW8/hw8.java
+++ HW/HW8/hw8.java
@@ -1,5 +1,18 @@
 package HW.HW8;
 
 public class hw8 {
-    
+    public int longestPalindrome(String s) {
+        int count = 0;
+
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        hw8 test = new hw8();
+        String p1 = "abccccdd" ; //expeted output 7
+        String p2 = "speediskey";  //expected output 5
+        System.out.println(test.longestPalindrome(p1));
+        System.out.println(test.longestPalindrome(p2));
+    }
 }
---
+++ HW/HW8/hw8.py
@@ -0,0 +1,13 @@
+class hw8:
+    def longestPalindrome(self, s):
+        
+
+
+
+#announced test cases
+p1 = "abccccdd" #expeted output 7
+p2 = "speediskey" #expected output 5
+##end of announced test cases
+intervals0 = []
+print(hw8().longestPalindrome(p1))
+print(hw8().longestPalindrome(p2))
