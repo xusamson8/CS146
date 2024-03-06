986c9f50a6ca2ce37c15c51c0207ab0c19d60cb1
xusamson8 <xusamson14@gmail.com>
2024-03-06 13:07:13 -0800

    lab5 NOTES

--- Labs/Lab 5/lab5.java
+++ Labs/Lab 5/lab5.java
@@ -29,6 +29,9 @@ public class lab5 {
 
         if (root == null ) return true;
         ///min max helper function when looing at subtrees?
+        //uupper bound 5 (highest possible value in left subtree) vice versa
+        // that means if we see a value higher thean the upper bound in the left subtree its automatically false
+        //every time we look at a new subtree, update upper and lwor bounds
      }
 
 }
