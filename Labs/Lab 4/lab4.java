94d466fc6ea936c374756ccbf00d4205a4c712a0
xusamson8 <xusamson14@gmail.com>
2024-03-06 12:31:37 -0800

    lab4 commit

--- Labs/Lab 4/lab4.java
+++ Labs/Lab 4/lab4.java
@@ -1,18 +1,10 @@
-package Labs.Lab 4;
-
 public class lab4 {
     public class TreeNode {
-
         int val;
-   
         TreeNode left;
-   
         TreeNode right;
-   
         TreeNode() {}
-   
         TreeNode(int val) { this.val = val; }
-   
         TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
@@ -21,7 +13,38 @@ public class lab4 {
      }
 
     public TreeNode invertTree(TreeNode root) {
+        /* if (root == null) return null;
         TreeNode curr = root;
-        
+        TreeNode temp = curr.left;
+        curr.left = curr.right;
+        curr.right = temp;
+        root.left = invertTree(root.right);
+        root.right = invertTree(root.left);
+        return root; */
+
+        /*
+         public class Solution {
+        public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+            TreeNode tmp = root.left;
+            root.left = invertTree(root.right);
+            root.right = invertTree(tmp);
+            return root;
+        }}
+         */
+
+         if(root == null) return null; //can't check children if it doesn texist, if we do recursion we shd check base case
+         if (root.right ==null && root.left == null) return root; //leaf node
+         if(root.left != null){
+            root.left = invertTree(root.left);
+         }
+         if(root.right != null){
+            root.right = invertTree(root.right);
+         }
+         TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            return root;
+        }
     }
 }
