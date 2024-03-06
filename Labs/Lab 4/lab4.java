7577861502f1dc46cfb74793786ef97cd172c607
xusamson8 <xusamson14@gmail.com>
2024-03-06 12:09:54 -0800

    lab4 creation

---
+++ Labs/Lab 4/lab4.java
@@ -0,0 +1,27 @@
+package Labs.Lab 4;
+
+public class lab4 {
+    public class TreeNode {
+
+        int val;
+   
+        TreeNode left;
+   
+        TreeNode right;
+   
+        TreeNode() {}
+   
+        TreeNode(int val) { this.val = val; }
+   
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+     }
+
+    public TreeNode invertTree(TreeNode root) {
+        TreeNode curr = root;
+        
+    }
+}
