2bb4ee3adf0f4eaf67a23adeefec787120bcd61f
xusamson8 <xusamson14@gmail.com>
2024-03-19 00:34:05 -0700

    hw11 commit

--- HW/HW11/hw11.py
+++ HW/HW11/hw11.py
@@ -1,4 +1,23 @@
 class hw11:
     def floodFill(self, image, sr, sc, newColor):
-    #Your code here
-        # optimize run time?
+        if image[sr][sc] == newColor:
+            return image
+        self.fill(image, sr, sc, image[sr][sc], newColor)
+        return image
+
+    def fill(self, image, sr, sc, color, newColor):
+        # Check for out of bounds indices
+        if sr < 0 or sr >= len(image) or sc < 0 or sc >= len(image[0]):
+            return
+        
+        # Check if the current color is the one we need to change
+        if image[sr][sc] == color:
+            image[sr][sc] = newColor
+            # Recursively call fill() for all adjacent pixels
+            self.fill(image, sr + 1, sc, color, newColor)
+            self.fill(image, sr - 1, sc, color, newColor)
+            self.fill(image, sr, sc + 1, color, newColor)
+            self.fill(image, sr, sc - 1, color, newColor)
+
+
+    # can optimize run time
