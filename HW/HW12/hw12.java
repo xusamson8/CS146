82cd3a927cef080c1f79b6c2456e6e3a616baebb
xusamson8 <xusamson14@gmail.com>
2024-04-10 17:42:11 -0700

    commit

--- HW/HW12/hw12.java
+++ HW/HW12/hw12.java
@@ -1,6 +1,15 @@
+
+
 public class hw12 {
+    
     public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
-        // Your code here
- 
-     }
+    }
+    public static void main(String[] args) {
+        hw12 solution = new hw12();
+        int n = 2;
+        int[] wells = {1, 1};
+        int[][] pipes = {{1, 2, 1}, {1, 2, 2}};
+        int result = solution.minCostToSupplyWater(n, wells, pipes);
+        System.out.println("Minimum cost to supply water: " + result);
+    }
 }
