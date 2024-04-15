898aaaaced23d547662493959acedf958c7d1c44
xusamson8 <xusamson14@gmail.com>
2024-04-15 00:26:21 -0700

    hw12 commit

--- HW/HW12/hw12.java
+++ HW/HW12/hw12.java
@@ -1,25 +1,35 @@
 import java.util.Arrays;
 
 public class hw12 {
+    // keeps track of root parents
     private int[] p;
 
     public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
+        // array hold copy of pipes in addition to space for the wells 
         int[][] nums = Arrays.copyOf(pipes, pipes.length + n);
+        //virtual pipes for wells, connects well i to house i with cost of well[i]
         for (int i = 0; i < n; i++) {
             nums[pipes.length + i] = new int[] {0, i + 1, wells[i]};
         }
+
+        //sorts costs for edges which are the pipes and wirtual well pipes 
         Arrays.sort(nums, (a, b) -> a[2] - b[2]);
+
+        //each node becomes own parent?
         p = new int[n + 1];
         for (int i = 0; i <= n; i++) {
             p[i] = i;
         }
         int ans = 0;
+        //loop all edges, find and compute costs
         for (var x : nums) {
             int a = x[0], b = x[1], c = x[2];
             int pa = find(a), pb = find(b);
+            // if the parents are different, combine and add costs
             if (pa != pb) {
                 p[pa] = pb;
                 ans += c;
+                //decerment edges, if 0 then we stop
                 if (--n == 0) {
                     return ans;
                 }
@@ -27,14 +37,16 @@ public class hw12 {
         }
         return ans;
     }
-
+    //find root of parent
     private int find(int x) {
+        //if its not its root parent, fidn its root parent
         if (p[x] != x) {
             p[x] = find(p[x]);
         }
         return p[x];
     }
     public static void main(String[] args) {
+        //given test case, expected output:2
         hw12 solution = new hw12();
         int n = 2;
         int[] wells = {1, 1};
--- HW/HW12/hw12.py
+++ HW/HW12/hw12.py
@@ -2,31 +2,42 @@ from typing import List
 
 class hw12:
   def minCostToSupplyWater(self, n: int, wells: List[int], pipes: List[List[int]]) -> int:
+    #functions finds root of an element
     def find(x: int) -> int:
       if p[x] != x:
         p[x] = find(p[x])
       return p[x]
 
     for i, w in enumerate(wells, 1):
+      #virtual edge from 0 node , with cost of well in house
       pipes.append([0, i, w])
+    #sorts edges with costs
     pipes.sort(key=lambda x: x[2])
+    #find sturcture initializeiation, each node is its own parent
     p = list(range(n + 1))
     ans = 0
+    #go over each pipe iterating
     for a, b, c in pipes:
-      pa, pb = find(a), find(b)
+      pa, pb = find(a), find(b) #find the root of its parents
       if pa != pb:
+        #if diff, add cost 
         p[pa] = pb
+        #decrement # of nodes if connected
         n -= 1
         ans += c
+        #if there are no more nodes left aka all connected 
         if n == 0:
           return ans
+ 
           
-  def test_cases():
+  def test_cases(self):
       solution = hw12()
-      
       #given test case
-      n1 = 2
-      wells1 = [1, 1]
-      pipes1 = [[1, 2, 1], [1, 2, 2]]
-      print("Test Case 1 Output:", solution.minCostToSupplyWater(n1, wells1, pipes1))
+      n = 2
+      wells = [1, 1]
+      pipes = [[1, 2, 1], [1, 2, 2]]
+      print("Test Case Output:", solution.minCostToSupplyWater(n, wells, pipes))
       #expected output: 2
+
+if __name__ == "__main__":
+    hw12().test_cases()
