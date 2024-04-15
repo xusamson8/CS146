36c89163c9acfb30f7a25a20e9eef6e4331a560f
xusamson8 <xusamson14@gmail.com>
2024-04-14 23:52:16 -0700

    hw 12 commit

--- HW/HW12/hw12.java
+++ HW/HW12/hw12.java
@@ -1,9 +1,38 @@
-
+import java.util.Arrays;
 
 public class hw12 {
-    
+    private int[] p;
+
     public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
-        
+        int[][] nums = Arrays.copyOf(pipes, pipes.length + n);
+        for (int i = 0; i < n; i++) {
+            nums[pipes.length + i] = new int[] {0, i + 1, wells[i]};
+        }
+        Arrays.sort(nums, (a, b) -> a[2] - b[2]);
+        p = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            p[i] = i;
+        }
+        int ans = 0;
+        for (var x : nums) {
+            int a = x[0], b = x[1], c = x[2];
+            int pa = find(a), pb = find(b);
+            if (pa != pb) {
+                p[pa] = pb;
+                ans += c;
+                if (--n == 0) {
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+
+    private int find(int x) {
+        if (p[x] != x) {
+            p[x] = find(p[x]);
+        }
+        return p[x];
     }
     public static void main(String[] args) {
         hw12 solution = new hw12();
--- HW/HW12/hw12.py
+++ HW/HW12/hw12.py
@@ -1,3 +1,32 @@
+from typing import List
+
 class hw12:
-    def minCostToSupplyWater(self, n: int, wells: List[int], pipes: List[List[int]]) -> int:
-  #Your code here
+  def minCostToSupplyWater(self, n: int, wells: List[int], pipes: List[List[int]]) -> int:
+    def find(x: int) -> int:
+      if p[x] != x:
+        p[x] = find(p[x])
+      return p[x]
+
+    for i, w in enumerate(wells, 1):
+      pipes.append([0, i, w])
+    pipes.sort(key=lambda x: x[2])
+    p = list(range(n + 1))
+    ans = 0
+    for a, b, c in pipes:
+      pa, pb = find(a), find(b)
+      if pa != pb:
+        p[pa] = pb
+        n -= 1
+        ans += c
+        if n == 0:
+          return ans
+          
+  def test_cases():
+      solution = hw12()
+      
+      #given test case
+      n1 = 2
+      wells1 = [1, 1]
+      pipes1 = [[1, 2, 1], [1, 2, 2]]
+      print("Test Case 1 Output:", solution.minCostToSupplyWater(n1, wells1, pipes1))
+      #expected output: 2
